934e2f0df5046d86c1efa911b7996fa2a5896f37
ElingVoon <72539062+ElingVoon@users.noreply.github.com>
2021-06-12 14:43:55 -0700

    Add files via upload
    parse api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-
   <div class="">
     <h1>Seattle Weather</h1>
-  
-
-
   </div>
   <div>
     <h1>Air Pollution</h1>
     <button v-on:click="getWeatherData">Air Pollution Data</button>
-
 <!-- <div v-for="data in getWeatherData" :key="list.main"> -->
   <!-- <p>{{ airPollutionList }}</p> -->
     <p>{{ airPollutionList }}</p>
+    <p>Air quality Index: {{ aqi }}</p>
+    <p>PM2.5 (Fine particles matter): {{ pm2_5 }}</p>
+    <p>PM10 (Coarse particulate matter): {{ pm10 }}</p>
 </div>
 </template>
 
@@ -21,20 +19,26 @@ export default {
   name: 'Home',
   data() {
     return {
-      airPollutionList: []
-    };
+      aqi: "" ,
+      pm2_5: "" ,
+      pm10: ""
+    }
   },
-  methods: {
-    getWeatherData(){
-      fetch('http://api.openweathermap.org/data/2.5/air_pollution?lat=47&lon=-122&appid=edceab3dc505dc66289d3d18b0b1b542')
-      .then (response => {
-        return response.json()
+
+      mounted() {
+        fetch('http://api.openweathermap.org/data/2.5/air_pollution?lat=47&lon=-122&appid=edceab3dc505dc66289d3d18b0b1b542')
+        .then (response => {
+          return response.json()
+          })
+          .then ((json) => {
+            if (json.list.length < 1) {
+              //TODO: show error or populate with dummy data - API retruned no data
+            }
+            this.aqi = json.list[0].main.aqi
+            this.pm2_5 = json.list[0].components.pm2_5
+            this.pm10 = json.list[0].components.pm10
+
         })
-        .then ((json) => {
-          this.airPollutionList = json;
-        })
-          .catch((err) => alert("ERROR", err));
-      }
     }
   }
 </script>
